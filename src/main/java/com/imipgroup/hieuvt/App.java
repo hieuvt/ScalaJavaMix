de8aa2b971a7efb7507b91f32910fb55f71cfec4
unknown <hieu.vutrong@DELLINS02.IMIPGROUP.COM>
2014-11-03 10:49:18 +0700

    first commit

---
+++ src/main/java/com/imipgroup/hieuvt/App.java
@@ -0,0 +1,14 @@
+package com.imipgroup.hieuvt;
+
+import com.imipgroup.hieuvt.scala.HelloWorld;
+
+/**
+ * Created by hieu.vutrong on 11/2/2014.
+ */
+public class App {
+    public static void main( String[] args )
+    {
+        new HelloWorld().show();
+        System.out.println("Hello, the Java world!");
+    }
+}
---
+++ src/main/scala/com/imipgroup/hieuvt/scala/HelloWorld.scala
@@ -0,0 +1,11 @@
+package com.imipgroup.hieuvt.scala
+
+/**
+ * Created by hieu.vutrong on 11/2/2014.
+ */
+class HelloWorld {
+
+  def show() {
+    println("Hello, the Scala world!")
+  }
+}
